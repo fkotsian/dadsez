188ae7b76d3fc67242b66544e36f21d36e172056
Frank Kotsianas <frankkotsianas@gmail.com>
2017-12-15 20:56:00 -0800

    BottomFive uses JokeLoader

--- src/containers/BottomFiveJokes/index.jsx
+++ src/containers/BottomFiveJokes/index.jsx
@@ -1,12 +1,8 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
-import Joke from '../../containers/Joke'
-import axios from 'axios'
+import JokeLoader from '../../containers/JokeLoader'
 import * as Consts from '../../constants.js'
-
-function canIHazAPIJokeUrl(canIHazId) {
-  return `${Consts.CANIHAZ_API_BASE_URL}/j/${canIHazId}`
-}
+import axios from 'axios'
 
 class BottomFiveJokes extends Component {
   constructor(props) {
@@ -18,7 +14,6 @@ class BottomFiveJokes extends Component {
     }
 
     this.requestBottomFiveJokes = this.requestBottomFiveJokes.bind(this)
-    this.fetchJokeInfo = this.fetchJokeInfo.bind(this)
   }
 
   componentDidMount() {
@@ -34,13 +29,8 @@ class BottomFiveJokes extends Component {
       Consts.DADSEZ_API_JOKES_BOTTOM_FIVE
     )
       .then(res => {
-        return res.data.joke_ids
-      })
-      .then(jokeIds => {
-        return this.fetchJokeInfo(jokeIds)
-      })
-      .then(_ => {
         this.setState({
+          jokes: res.data.jokes,
           loading: false,
         })
       })
@@ -50,24 +40,6 @@ class BottomFiveJokes extends Component {
           loading: false,
         })
       })
-  }
-
-  fetchJokeInfo(jokeIds) {
-    jokeIds.map(jId => {
-      // request joke from API and add it to the list
-      axios.get(
-        canIHazAPIJokeUrl(jId),
-        {
-          headers: Consts.CANIHAZ_API_HEADERS,
-        }
-      )
-        .then(res => {
-          let newJoke = res.data
-          this.setState({
-            jokes: [].concat(this.state.jokes).concat(newJoke),
-          })
-        })
-    })
   }
 
   render() {
@@ -93,9 +65,10 @@ class BottomFiveJokes extends Component {
         {
           this.state.jokes.length > 0
             ?
-              this.state.jokes.map(j => (
-                <Joke key={j.id} {...j} />
-              ))
+              this.state.jokes
+                .map(j => (
+                  <JokeLoader key={j.id} {...j} />
+                ))
             :
               ''
         }
